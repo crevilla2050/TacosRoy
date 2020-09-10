a6fa36606e6f3b1e9d882e50320d5b3d25883c25
Carlos Revilla <carlos.revilla.m@gmail.com>
2020-09-10 17:27:45 -0500

    Actualización a 2.0, generic files
    Archivos que son base para el funcionamiento de la versión 2.0 de la app
    Tacos Roy restaurant

--- viewsAndCodes.sql
+++ viewsAndCodes.sql
@@ -4,7 +4,7 @@ SELECT
 `tbl_subcategorias`.`chr_nom_subcat` ,
 `tbl_subcategorias`.`id_subcategoria` 
 FROM  `tbl_subcategorias` 
-RIGHT JOIN  `db_tacos_roy`.`tbl_categorias` ON  `tbl_subcategorias`.`id_cat_padre` =  `tbl_categorias`.`id_categoria` 
+RIGHT JOIN  `db_tacosRoy`.`tbl_categorias` ON  `tbl_subcategorias`.`id_cat_padre` =  `tbl_categorias`.`id_categoria` 
 
 CREATE VIEW  `vw_datos_usuarios` AS 
 SELECT  
@@ -37,7 +37,7 @@ SELECT
 `tbl_categorias`.`chr_desc_cat` as 'Tooltip',
 `tbl_productos`.`int_activo` as 'Activo'
 FROM `tbl_productos`
- LEFT JOIN `db_tacos_roy`.`tbl_categorias` ON `tbl_productos`.`id_categoria` = `tbl_categorias`.`id_categoria` 
+ LEFT JOIN `db_tacosRoy`.`tbl_categorias` ON `tbl_productos`.`id_categoria` = `tbl_categorias`.`id_categoria` 
 
 Create view `vw_products_catprecio` AS
 SELECT 
@@ -51,8 +51,8 @@ SELECT
 `tbl_tipos_precios`.`chr_nombre_precio` as 'Tipo',
 `tbl_productos`.`int_activo` as 'Activo'
 FROM `tbl_productos`
- LEFT JOIN `db_tacos_roy`.`tbl_categorias` ON `tbl_productos`.`id_categoria` = `tbl_categorias`.`id_categoria`
- LEFT JOIN `db_tacos_roy`.`tbl_precios_productos` ON `tbl_productos`.`id_producto` = `tbl_precios_productos`.`id_producto`
+ LEFT JOIN `db_tacosRoy`.`tbl_categorias` ON `tbl_productos`.`id_categoria` = `tbl_categorias`.`id_categoria`
+ LEFT JOIN `db_tacosRoy`.`tbl_precios_productos` ON `tbl_productos`.`id_producto` = `tbl_precios_productos`.`id_producto`
  LEFT JOIN `tbl_tipos_precios` ON `tbl_tipos_precios`.`id_tipo_precio` = `tbl_precios_productos`.`int_tipo_precio`
  
 
@@ -68,18 +68,18 @@ SELECT
 `tbl_prods_x_orden`.`dt_horafecha_pedido` as 'HoraFecha',
 `tbl_prods_x_orden`.`bool_activo` as 'Activo'
 FROM `tbl_ordenes`
- LEFT JOIN `db_tacos_roy`.`tbl_prods_x_orden` ON `tbl_ordenes`.`id_orden_id` = `tbl_prods_x_orden`.`int_orden_id` 
- LEFT JOIN `db_tacos_roy`.`tbl_productos` ON `tbl_prods_x_orden`.`int_producto_id` = `tbl_productos`.`id_producto`
- LEFT JOIN `db_tacos_roy`.`tbl_precios_productos` on `tbl_precios_productos`.`id_producto` = `tbl_productos`.`id_producto`
- LEFT JOIN `db_tacos_roy`.`tbl_consecutivo` on `tbl_ordenes`.`int_consecutivo` = `tbl_consecutivo`.`id_consecutivo`
+ LEFT JOIN `db_tacosRoy`.`tbl_prods_x_orden` ON `tbl_ordenes`.`id_orden_id` = `tbl_prods_x_orden`.`int_orden_id` 
+ LEFT JOIN `db_tacosRoy`.`tbl_productos` ON `tbl_prods_x_orden`.`int_producto_id` = `tbl_productos`.`id_producto`
+ LEFT JOIN `db_tacosRoy`.`tbl_precios_productos` on `tbl_precios_productos`.`id_producto` = `tbl_productos`.`id_producto`
+ LEFT JOIN `db_tacosRoy`.`tbl_consecutivo` on `tbl_ordenes`.`int_consecutivo` = `tbl_consecutivo`.`id_consecutivo`
 
 Create view `vw_inventarios_fecha` as SELECT 
 `tbl_inventario`.`id_inventario_id` as 'InvID', 
 `tbl_insumos`.`chr_nombre_insumo` as 'Insumo', 
 `tbl_inventario`.`int_cantidad` as 'Cantidad', 
 `tbl_inventario`.`dt_fecha_insumo` as 'Fecha'
-FROM `db_tacos_roy`.`tbl_inventario`
-LEFT JOIN `db_tacos_roy`.`tbl_insumos` ON `tbl_inventario`.`id_insumo_id` = `tbl_insumos`.`id_insumo`
+FROM `db_tacosRoy`.`tbl_inventario`
+LEFT JOIN `db_tacosRoy`.`tbl_insumos` ON `tbl_inventario`.`id_insumo_id` = `tbl_insumos`.`id_insumo`
 
 CREATE VIEW `vw_variantes_x_categoria` AS 
 SELECT 
@@ -88,9 +88,9 @@ SELECT
 `tbl_categorias`.`chr_nombre_cat` as 'Categoria',
 `tbl_variantes_platillos`.`id_variante_pl` as 'IDVAR',
 `tbl_variantes_platillos`.`chr_variante_nombre` as 'Variante'
-FROM `db_tacos_roy`.`tbl_variantes_categorias`     
-LEFT JOIN `db_tacos_roy`.`tbl_categorias` ON `tbl_variantes_categorias`.`id_catgoria` = `tbl_categorias`.`id_categoria` 
-LEFT JOIN `db_tacos_roy`.`tbl_variantes_platillos` ON `tbl_variantes_platillos`.`id_variante_pl` = `tbl_variantes_categorias`.`id_variante_platillo`  
+FROM `db_tacosRoy`.`tbl_variantes_categorias`     
+LEFT JOIN `db_tacosRoy`.`tbl_categorias` ON `tbl_variantes_categorias`.`id_catgoria` = `tbl_categorias`.`id_categoria` 
+LEFT JOIN `db_tacosRoy`.`tbl_variantes_platillos` ON `tbl_variantes_platillos`.`id_variante_pl` = `tbl_variantes_categorias`.`id_variante_platillo`  
 
 CREATE VIEW `vw_opciones_platillos_ticket` AS
 SELECT
@@ -98,7 +98,7 @@ SELECT
 `tbl_prods_orden_opciones`.`id_prod_x_orden` as 'IDPO', 
 `tbl_variantes_platillos`.`chr_imprimir` AS 'TxtTicket'
 FROM `tbl_prods_orden_opciones` 
-LEFT JOIN `db_tacos_roy`.`tbl_variantes_platillos` ON `tbl_prods_orden_opciones`.`id_variante_platillo` = `tbl_variantes_platillos`.`id_variante_pl`
+LEFT JOIN `db_tacosRoy`.`tbl_variantes_platillos` ON `tbl_prods_orden_opciones`.`id_variante_platillo` = `tbl_variantes_platillos`.`id_variante_pl`
 
 CREATE VIEW `vw_insumos_usados_x_orden` AS
 SELECT 
@@ -146,7 +146,7 @@ FROM `tbl_personal`
 LEFT JOIN `tbl_puestos` ON `tbl_personal`.`int_puesto` = `tbl_puestos`.`id_puesto`  
 LEFT JOIN `tbl_usuarios_sistema` ON `tbl_personal`.`id_personal` = `tbl_usuarios_sistema`.`id_personal` 
 LEFT JOIN `tbl_usuarios` on `tbl_usuarios`.`id_usuario` = `tbl_usuarios_sistema`.`id_usuario` 
-LEFT JOIN `db_tacos_roy`.`tbl_status` ON `tbl_usuarios`.`int_status` = `tbl_status`.`id_status` 
+LEFT JOIN `db_tacosRoy`.`tbl_status` ON `tbl_usuarios`.`int_status` = `tbl_status`.`id_status` 
 
 CREATE VIEW `vw_ordenes_x_mesero` AS
 SELECT 
